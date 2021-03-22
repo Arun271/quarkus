0548345c0d6214a9ba63178364987688dd0e37f1
Arun271 <54939815+Arun271@users.noreply.github.com>
2021-03-22 19:16:25 +0530

    TODO1

--- Kogito Practice/src/main/java/org/acme/ExampleResource.java
+++ Kogito Practice/src/main/java/org/acme/ExampleResource.java
@@ -13,7 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+// TODO write these class in separate controller package
 @Path("/movies")
 
 public class ExampleResource {
